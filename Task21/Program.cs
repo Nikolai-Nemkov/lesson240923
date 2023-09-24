b704ad2c18b956a930d5735b52d61cc6c13624af
Николай <nnm_58@mail.ru>
2023-09-24 14:41:50 +0300

    Добавил папку Task23

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 21: Напишите программу, которая принимает
+// на вход координаты двух точек и находит 
+// расстояние между ними в 3D пространстве.
+
+Console.Write("Введите координаты точки x1: "); 
+int x1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите координаты точки x2: "); 
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты точки y1: "); 
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты точки y2: "); 
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите координаты точки z1: "); 
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты точки z2: "); 
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+double res = Distance(x1,x2,y1,y2,z1,z2); 
+
+Console.WriteLine($"{Math.Round(res, 2, MidpointRounding.ToZero)}");
+  
+double Distance(int xValue1, int xValue2, int yValue1, int yValue2,  int zValue1, int zValue2)
+{     
+    double result = Math.Sqrt(Math.Pow((xValue2 - xValue1), 2) + Math.Pow((yValue2 - yValue1), 2)
+    + Math.Pow((zValue2 - zValue1), 2));    
+    return result;  
+}
+
+
+
