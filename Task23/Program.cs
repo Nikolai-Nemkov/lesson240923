1f365c60c499a9dff86f9f2cfd35c4d038014b81
Николай <nnm_58@mail.ru>
2023-09-24 14:54:05 +0300

    Добавил папку Task22

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 22: Напишите программу, которая 
+// принимает на вход число (N) и выдаёт таблицу 
+// кубов чисел от 1 до N. 
+
+Console.Write("Введите число: "); 
+int num = Convert.ToInt32(Console.ReadLine()); 
+
+TableSquare(num); 
+
+void TableSquare(int number) 
+{     
+    int count = 1; 
+    while (count <= number)     
+    {         
+        Console.WriteLine($"{count, 3} -> {count * count * count, 5}");         
+        count += 1;     
+    } 
+}
+
